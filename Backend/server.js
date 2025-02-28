7b94c2c4ff59b8b7d6040d484d655de601757edd
tim177 <timaraw18@gmail.com>
2025-02-28 19:16:03 +0530

    final backend

--- Backend/server.js
+++ Backend/server.js
@@ -5,16 +5,19 @@ dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("DB connection successful"))
-  .catch((error) => console.log("error:", error));
+  .catch((error) => console.log("DB connection error:", error));
 
+// Use process.env.PORT for Render
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, "0.0.0.0", () => {
+const server = app.listen(PORT, "0.0.0.0", () => {
   console.log(`App running at port: ${PORT}...`);
 });
 
+// Handle Unhandled Promise Rejections
 process.on("unhandledRejection", (err) => {
   console.log(err.name, err.message);
   console.log("Unhandled Rejection 💥 shutting down...");
