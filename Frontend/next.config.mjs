f1e307e9b56f1b0bf0423f4b69a36150bd7cc69e
tim177 <timaraw18@gmail.com>
2025-02-28 20:13:38 +0530

    updated config

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -3,9 +3,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Ignore ESLint errors during builds
   },
-  output: "export", // Ensures Next.js generates static files
-  distDir: "out", // Render expects static output here
-  trailingSlash: true, // Ensures proper linking for static exports
+  output: "standalone", // Use standalone mode for Web Services
 };
 
 export default nextConfig;
