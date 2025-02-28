1ac71e2e184a744ac6c1631ba599dc717744343a
tim177 <timaraw18@gmail.com>
2025-02-28 19:07:58 +0530

    added port 0.0.0.0

--- Backend/server.js
+++ Backend/server.js
@@ -10,9 +10,9 @@ mongoose
   .then(() => console.log("DB connection successful"))
   .catch((error) => console.log("error:", error));
 
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
-  console.log(`App running at port: ${port}...`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`App running at port: ${PORT}...`);
 });
 
 process.on("unhandledRejection", (err) => {
